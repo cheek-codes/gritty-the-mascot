dc802c3ec5a3cd0ac9dad9856f49677a3c1777a4
cheek-codes <cheek.codes@gmail.com>
2023-03-26 21:47:11 -0400

    played around

--- assets/js/no-click.js
+++ assets/js/no-click.js
@@ -1,12 +1,35 @@
-const myElement = document.getElementById("no");
+const container = document.getElementById("nein")
+const myElement1 = document.getElementById("no")
 
-myElement.addEventListener("click", () => {
-  // Get a random position for the element
-  const x = Math.floor(Math.random() * window.innerWidth);
-  const y = Math.floor(Math.random() * window.innerHeight);
+// Add event listeners to both elements
+myElement1.addEventListener("click", moveElements)
 
-  // Set the position of the element
-  myElement.style.position = "absolute";
-  myElement.style.left = x + "px";
-  myElement.style.top = y + "px";
-});
+function moveElements() {
+  // Get random positions within the container bounds
+  const maxX = container.clientWidth - myElement1.clientWidth
+  const maxY = container.clientHeight - myElement1.clientHeight
+  const x1 = Math.floor(Math.random() * maxX)
+  const y1 = Math.floor(Math.random() * maxY)
+
+  // Set the position of each element
+  myElement1.style.left = x1 + "px"
+  myElement1.style.top = y1 + "px"
+
+}
+
+
+
+
+let count = 0
+
+function changeElement() {
+  count++
+  if (count === 5) {
+    const newElement = document.createElement("a")
+    newElement.href = "https://www.google.com"
+    newElement.textContent = "New link"
+    const container = document.getElementById("nein")
+    const oldElement = document.getElementById("no")
+    container.replaceChild(newElement, oldElement)
+  }
+}
