e2bc9b1a0bbcb9f40035fcc5b886d2f974adacce
cheek-codes <cheek.codes@gmail.com>
2023-03-28 21:34:39 -0400

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,17 @@
-var images = [
-  {image: "assets/images/giphy.gif", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"},
-  {image: "assets/images/1.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/2.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/3.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/4.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/5.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/6.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/7.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/8.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"}, 
-  {image: "assets/images/9.jpg", music: "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3"},
-  {image: "assets/images/momo.jpg", music: "assets/music/Little-Girl-Screaming-A2.mp3"},
-  {image: "assets/images/nope.jpg", music: "assets/music/Scream-Short-B5.mp3"},
-]
-var currentIndex = 0 // Index of the currently displayed image
-var defaultImage = "assets/images/giphy.gif"; // Default image file name
-var defaultMusic = "assets/music/fun-kids-playful-comic-carefree-game-happy-positive-music.mp3" // Default music file name
-var musicPlayer = document.getElementById("music-player") // Get the audio element
+const container = document.querySelector(".container")
+const link = document.querySelector(".link")
 
-// var audio = document.getElementsByTagName("audio")[0]
-// audio.play()
+// element moves
+link.addEventListener("load", move)
+move()
 
+function move() {
+    const maxX = container.clientWidth - link.clientWidth
+    const maxY = container.clientHeight - link.clientHeight
+    const x1 = Math.floor(Math.random() * maxX)
+    const y1 = Math.floor(Math.random() * maxY)
 
+    link.style.left = x1 + 'px'
+    link.style.top = y1 + 'px'
 
-
-// function changeImage() {
-//   var img = document.getElementById('my-image') // Get the image element
-//   var currentImage = images[currentIndex] // Get the current image and music file names
-  
-//   if (img.src.indexOf(defaultImage) !== -1) {
-//     // If the image is the default image, change to a specific image and play its corresponding music file
-//     var randomIndex = Math.floor(Math.random() * images.length)
-//     currentIndex = randomIndex; // Set the currentIndex to the index of the new image
-//     img.src = currentImage.image;
-//     musicPlayer.src = currentImage.music;
-//     musicPlayer.play() // Play the music
-//     setTimeout(function() {
-//       img.src = defaultImage; // Set the source of the image back to the default image after 2 seconds
-//       musicPlayer.src = defaultMusic; // Set the source of the music back to the default music file
-//       musicPlayer.play() // Stop the music
-//     }, 2000)
-//   } else {
-//     // If the image is not the default image, set it back to the default image and stop the music
-//     img.src = defaultImage
-//     musicPlayer.src = defaultMusic
-//     musicPlayer.play()
-//   }
-// }
-
-// setInterval(changeImage, Math.floor(Math.random() * (12000 - 5000 + 1) + 5000)); // Call the changeImage function at a random interval between 5 and 12 seconds
-
-// console.log("hi")
-
-
-
-
-function changeImage() {
-  var img = document.getElementById('my-image'); // Get the image element
-  var currentImage = images[currentIndex]; // Get the current image and music file names
-  
-  if (img.src.indexOf(defaultImage) !== -1) {
-    // If the image is the default image, change to a specific image and play its corresponding music file
-    var randomIndex = Math.floor(Math.random() * images.length);
-    currentIndex = randomIndex; // Set the currentIndex to the index of the new image
-    img.src = currentImage.image;
-    musicPlayer.src = currentImage.music;
-    musicPlayer.play(); // Play the music
-    setTimeout(function() {
-      img.src = defaultImage; // Set the source of the image back to the default image after 2 seconds
-      musicPlayer.src = defaultMusic; // Set the source of the music back to the default music file
-      musicPlayer.pause(); // Stop the music
-    }, 2000);
-  } else {
-    // If the image is not the default image, set it back to the default image and stop the music
-    img.src = defaultImage;
-    musicPlayer.pause(); // Stop the music
-    if (currentImage.music !== defaultMusic) {
-      // If the image being replaced has a different music file, stop it before playing the default music file
-      musicPlayer.src = defaultMusic;
-      musicPlayer.play(); // Play the default music file
-    }
-  }
 }
-setInterval(changeImage, Math.floor(Math.random() * (12000 - 5000 + 1) + 5000)); // Call the changeImage function at a random interval between 5 and 12 seconds
--- assets/js/no-click.js
+++ assets/js/no-click.js
@@ -2,34 +2,9 @@ const container = document.getElementById("nein")
 const link = document.getElementById("no")
 const heading = document.getElementById("change")
 const texts = document.getElementById("texts")
-const newText = ["Why you no listen again?", "STHAP!", "DON'T DO IT AGAIN!!!", "Quit your tomfoolery!", "Okay fine, you win. Here's your prize. Go ahead click it."]
+const newText = ["Why you clicked again?", "STHAP!", "DON'T DO IT AGAIN!!!", "Quit your tomfoolery!", "Okay fine, you win. Here's your prize. Go ahead click it."]
 let index = 0
 let count = 0
-
-link.addEventListener("click", function() {
-  heading.textContent = "New Heading Text"
-})
-
-// change to a link
-function changeElement() {
-    count++
-    if (count === 6) {
-        const newElement = document.createElement("a")
-        newElement.href = "https://www.google.com"
-        newElement.textContent = "New link"
-        const oldElement = document.getElementById("no")
-        const maxX = container.clientWidth - link.clientWidth
-        const maxY = container.clientHeight - link.clientHeight
-        const x1 = Math.floor(Math.random() * maxX)
-        const y1 = Math.floor(Math.random() * maxY)
-
-        newElement.style.position = "absolute"
-        newElement.style.left = x1 + 'px'
-        newElement.style.top = y1 + 'px'
-        container.appendChild(newElement)
-        container.removeChild(oldElement)
-    }
-}
 
 // element moves
 link.addEventListener('click', moveElements)
@@ -57,3 +32,41 @@ link.addEventListener("click", function() {
         index = 0
     }
 })
+
+// change to a link
+function changeElement() {
+    count++
+    if (count === 6) {
+        const rick_roll = document.createElement("a")
+        rick_roll.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        rick_roll.textContent = "Click here"
+        const oldElement = document.getElementById("no")
+        const maxX = container.clientWidth - link.clientWidth
+        const maxY = container.clientHeight - link.clientHeight
+        const x1 = Math.floor(Math.random() * maxX)
+        const y1 = Math.floor(Math.random() * maxY)
+
+        rick_roll.style.position = "absolute"
+        rick_roll.style.left = x1 + "px"
+        rick_roll.style.top = y1 + "px"
+        container.appendChild(rick_roll)
+        container.removeChild(oldElement)
+    }
+    if (count === 6){
+        const penguins = document.createElement("a")
+        penguins.href = "penguins_rule.html"
+        penguins.textContent = "DO NOT CLICK HERE"
+
+        const oldElement = document.getElementById("no")
+        const maxX = container.clientWidth - link.clientWidth
+        const maxY = container.clientHeight - link.clientHeight
+        const x1 = Math.floor(Math.random() * maxX)
+        const y1 = Math.floor(Math.random() * maxY)
+
+        penguins.style.position = "absolute"
+        penguins.style.left = x1 + "px"
+        penguins.style.top = y1 + "px"
+        container.appendChild(penguins)
+        container.removeChild(oldElement)
+    }
+}
