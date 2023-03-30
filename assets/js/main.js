737ae2a22b11f0a72c8b888fa833284338f0b593
cheek-codes <cheek.codes@gmail.com>
2023-03-29 22:14:07 -0400

    js and css updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 const container = document.querySelector(".container")
 const link = document.querySelectorAll(".link")
+const jail = document.querySelector("#jail")
+const wendys = document.querySelector("#wendys")
+const ramen = document.querySelector("#ramen")
+const noods = document.querySelector("#noods")
+const text = document.querySelector(".noodz")
+const newText = ["JK. Here's some noods", "Do you really want noods", "Sorry, here's actual noods."]
+let index = 0
+let count = 0
+
 
 // element moves
 document.addEventListener("DOMContentLoaded", move);
@@ -15,3 +24,28 @@ function move() {
         link.style.top = y1 + 'px'
     })
 }
+
+
+// toggle images
+document.querySelector(".noodz").addEventListener("click", next1)
+document.querySelector(".noodz").addEventListener("click", next2)
+document.querySelector(".noodz").addEventListener("click", next3)
+document.querySelector(".noodz").addEventListener("click", next4)
+
+function next1(){
+    jail.classList.toggle("hidden")
+    wendys.classList.add("hidden")
+    ramen.classList.add("hidden")
+    noods.classList.add("hidden")
+}
+
+// text change
+text.addEventListener("click", function() {
+    if (index < newText.length) {
+        text.textContent = newText[index]
+        index++
+    } 
+    else {
+        index = 0
+    }
+})
